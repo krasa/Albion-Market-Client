fb2166127ce915228658f64a862191ad1df18b91
Vojtěch Krása <vojta.krasa@gmail.com>
2024-01-04 19:59:13 +0100

    update

--- src/test/java/krasa/albion/LauncherTests.java
+++ src/test/java/krasa/albion/LauncherTests.java
@@ -2,12 +2,12 @@ package krasa.albion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class LauncherTests {
-	@Test
-	void contextLoads() {
-		// do nothing
-	}
-
-}
+//
+//@SpringBootTest
+//class LauncherTests {
+//	@Test
+//	void contextLoads() {
+//		// do nothing
+//	}
+//
+//}
